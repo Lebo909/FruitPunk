f49763c811415b746c56d997b3952b0f346ed68c
John Pike <japikesphs@gmail.com>
2020-01-06 21:03:15 -0500

    Updated in accordance with backend

--- DISCORD/index.php
+++ DISCORD/index.php
@@ -16,7 +16,7 @@ die("Connection failed: " . $conn->connect_error);
 
 include('arrays.php');
 
-$result = mysqli_query($conn, "SELECT * FROM song_queue;");
+$result = mysqli_query($conn, "SELECT * FROM song_queue WHERE status=0;");
 
 $rmeme=array_rand($memes, 3);
 
@@ -31,7 +31,7 @@ $rmeme1=array_rand($memes1, 3);
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<title><?php echo $memes1[$rmeme1[0]];?></title>
+		<title>Fruit Punk</title>
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 		<!-- Bootstrap core CSS -->
@@ -69,29 +69,40 @@ $rmeme1=array_rand($memes1, 3);
 		<script src="js/wow.min.js"></script>
 		<script> new WOW().init(); </script>
 
-			<script>
-			function loadXMLDoc() {
-				var xmlhttp;
-				if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-					xmlhttp = new XMLHttpRequest();
-				} else { // code for IE6, IE5
-					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				xmlhttp.onreadystatechange = function () {
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-					}
-				}
-				xmlhttp.open("POST", "http://0.0.0.0:5000/postmethod", true);
-				xmlhttp.send(JSON.stringify({"type":"new", "url":document.getElementById("search").value}));
-			}
-		</script>
-
 		<link rel="icon" href='crylaugh.ico'>
+
+	<header>
+		<!--Navbar-->
+		<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+		  <div class="container">
+			<img class="px-2"  src="fpgood.png" alt="logo" width="49">
+			<a class="nava navbar-brand" href="#"><strong>fp</strong></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
+			  aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+			  <ul class="navbar-nav px-3">
+				<li class="nav-item active">
+				  <a class="nav-link" href="index.php">Queue <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item px-3">
+				  <a class="nav-link" href="playlists.php">Playlists</a>
+				</li>
+			  </ul>
+			  <ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+				  <a class="nav-link pl-5" href="#">© <?php echo $memes1[$rmeme1[0]];?> 2020</a>
+				</li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+	</header>
 
 		<!-- Title Bar -->
 
-		<div class="container-fluid grey darken-3">
+		<div class="container-fluid grey darken-3 pt-5">
 			<div class="row flex-center">
 				<!-- Title -->
 				<img class="animated tada" src="punk.png" alt="logo" width="100">
@@ -102,8 +113,47 @@ $rmeme1=array_rand($memes1, 3);
 			<?php echo '<h3 style="color:#00d277;" class="text text-center animated fadeIn">"'.$memes[$rmeme[0]].'"</h3>';?>
 		</div>
 
+		<?php
+			function http($url, $data = [], $method = 'get'){
+
+				$ch = curl_init();
+				$chOpts = [
+					CURLOPT_SSL_VERIFYPEER => false,
+					CURLOPT_HEADER => false,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_CONNECTTIMEOUT => 8,
+					CURLOPT_TIMEOUT => 16,
+					CURLOPT_HTTPHEADER, [
+						'Content-Type: application/json'
+					]
+				];
+
+				if ($method == 'post') {
+					$chOpts[CURLOPT_POST] = true;
+					$chOpts[CURLOPT_POSTFIELDS] = $data;
+					$chOpts[CURLOPT_URL] = $url;
+				}
+				else {
+					$url.='?'.is_array($data)?http_build_query($data):$data;
+					$chOpts[CURLOPT_URL] = $url;
+				}
+
+				curl_setopt_array($ch, $chOpts);
+				$response = curl_exec($ch);
+				curl_close($ch);
+				return $response;
+			}
+			
+			if(isset($_POST['search']))
+			{
+				$url = 'http://localhost:5000/postmethod';
+				http($url, json_encode(["type" => "new", "url" => $_POST['search']]), 'post');
+			} 
+		?>
+
 		<!-- URL Bar and POST -->
-		<form action="index.php" method="POST" role="form" onsubmit="loadXMLDoc()">	  
+		<form action="index.php" method="POST" role="form">	  
 			<div class="form-group container flex-center pt-3">
 				<div class="md-form search col-lg-8">
 					<input class="form-control text-center" type="text" id="search" name="search" placeholder="Enter YouTube URL to Add Video" aria-label="Search">
--- DISCORD/queue.php
+++ DISCORD/queue.php
@@ -8,69 +8,100 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <!-- Material Design Bootstrap -->
-  <link rel="stylesheet" href="css/mdb.min.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title></title>
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Material Design Bootstrap -->
+	<link rel="stylesheet" href="css/mdb.min.css">
 
-  <!-- Your custom styles (optional) -->
-  <style>
+	<!-- Your custom styles (optional) -->
+	<style>
 
-  input, textarea{
-    background-color:#666;
-    color: #FFF;
-	}
+	</style>
+
+
+	<!-- SCRIPTS -->
+	<!-- JQuery -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+	<!-- Bootstrap tooltips -->
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<!-- MDB core JavaScript -->
+	<script type="text/javascript" src="js/mdb.min.js"></script>
+	<!-- YouTube API -->
+	<script src= "https://apis.google.com/js/client.js"></script>
+
 	
-  </style>
-
-
-  <!-- SCRIPTS -->
-  <!-- JQuery -->
-  <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
-  <!-- YouTube API -->
-  <script src= "https://apis.google.com/js/client.js"></script>
-
-  
-  <!-- Animations -->
-  <link rel="stylesheet" href="css/animate.css">
-  <script src="js/wow.min.js"></script>
-  <script> new WOW().init(); </script>
+	<!-- Animations -->
+	<link rel="stylesheet" href="css/animate.css">
+	<script src="js/wow.min.js"></script>
+	<script> new WOW().init(); </script>
 
 </head>
 <body>
-              <!-- Grid row -->
-              <div class="row">
+				<!-- Grid row -->
+				<div class="row">
 
-                <!-- Grid column -->
-                <div class="col-md-12">
+				<!-- Grid column -->
+				<div class="col-md-12">
 
 				<!-- Card -->
-                  <div class="card grey darken-2">
+					<div class="card grey darken-2">
 
-                    <!-- Card content -->
-                    <div class="card-body px-3 grey darken-2">
-                      
-						  <!-- Title -->
-						  <h3 class="card-title text-center" style="color:#00d277;">Queue</h3>
-						  <hr class="grey darken-4 mb-5">
+					<!-- Card content -->
+					<div class="card-body px-3 grey darken-2">
+						
+							<!-- Title -->
+							<h3 class="card-title text-center" style="color:#00d277;">Queue</h3>
+							<hr class="grey darken-4 mb-5">
 						
 							<ol class="example">
 
 								<?php
-										
+								function httpdel($url, $data = [], $method = 'get'){
+
+									$ch = curl_init();
+									$chOpts = [
+										CURLOPT_SSL_VERIFYPEER => false,
+										CURLOPT_HEADER => false,
+										CURLOPT_FOLLOWLOCATION => true,
+										CURLOPT_RETURNTRANSFER => true,
+										CURLOPT_CONNECTTIMEOUT => 8,
+										CURLOPT_TIMEOUT => 16,
+										CURLOPT_HTTPHEADER, [
+											'Content-Type: application/json'
+										]
+									];
+
+									if ($method == 'post') {
+										$chOpts[CURLOPT_POST] = true;
+										$chOpts[CURLOPT_POSTFIELDS] = $data;
+										$chOpts[CURLOPT_URL] = $url;
+									}
+									else {
+										$url.='?'.is_array($data)?http_build_query($data):$data;
+										$chOpts[CURLOPT_URL] = $url;
+									}
+
+									curl_setopt_array($ch, $chOpts);
+									$response = curl_exec($ch);
+									curl_close($ch);
+									return $response;
+								}
+
+								if(isset($_POST['delete']))
+								{
+									$url = 'http://localhost:5000/postmethod';
+									http($url, json_encode(["type" => "delete", "id" => $_POST['delete']]), 'post');
+								} 
+
 								while($row = mysqli_fetch_array($result))
 								{
 									
@@ -112,10 +143,12 @@
 													</div>
 													<div class="col-lg-8 my-auto mx-auto">
 														<a href="'.$row['url'].'"><t style="color:#ffffff;" class="text">'.$row['title'].'</t></a>
-													</div>
+													</div>													
 													<div class="col-lg-2 my-auto text-right">
+													<form action="index.php" id="delForm" method="post" role="form">
 														<t style="color:#ffffff;" class="text">'.$dur.'</t>
-														<a href="#"><i class="fas fa-times-circle white-text ml-5"></i></a>
+														    <button type="submit" id="delete" name="delete" value='.$vcount.' class="fas fa-times-circle white-text ml-5" style="border:0px transparent; background-color: transparent;" ></button>
+													</form>
 													</div>										
 												</div>
 											</div>
@@ -130,23 +163,31 @@
 							<br>
 						
 					
-                    </div>
-                    <!-- Card content -->
+					</div>
+					<!-- Card content -->
 
-                  </div>
-                  <!-- Card -->
+					</div>
+					<!-- Card -->
 
-                </div>
-                <!-- Grid column -->
+				</div>
+				<!-- Grid column -->
 
-              </div>
-              <!-- Grid row -->
+				</div>
+				<!-- Grid row -->
 
 	<script>
-	    $(document).ready(function() {
-	        $("ol.example").sortable({
-	            update:function(event, ui){
-	                alert("in the update");
+		$(document).ready(function(){
+		    $("a.fas.fa-times-circle.white-text.ml-5").click(function(){
+		        document.getElementById("delForm").submit();
+		    }); 
+		});
+	</script>
+
+	<script>
+		$(document).ready(function() {
+			$("ol.example").sortable({
+				update:function(event, ui){
+					alert("in the update");
 					var mydata = $(this).sortable('serialize');
 
 					// POST to server using $.post or $.ajax
@@ -155,9 +196,9 @@
 						type: 'POST',
 						url: 'http://localhost:5000/'
 					});
-	            }
-	        });
-	    });
+				}
+			});
+		});
 	</script>
 
 </body>
