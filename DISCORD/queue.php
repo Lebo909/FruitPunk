4bacb1ea676c006109c4ee0d8ad47096c3847818
John Pike <japikesphs@gmail.com>
2020-01-07 21:06:52 -0500

    Delete page post and redirect

--- DISCORD/queue.php
+++ DISCORD/queue.php
@@ -65,42 +65,6 @@
 							<ol class="example">
 
 								<?php
-								function httpdel($url, $data = [], $method = 'get'){
-
-									$ch = curl_init();
-									$chOpts = [
-										CURLOPT_SSL_VERIFYPEER => false,
-										CURLOPT_HEADER => false,
-										CURLOPT_FOLLOWLOCATION => true,
-										CURLOPT_RETURNTRANSFER => true,
-										CURLOPT_CONNECTTIMEOUT => 8,
-										CURLOPT_TIMEOUT => 16,
-										CURLOPT_HTTPHEADER, [
-											'Content-Type: application/json'
-										]
-									];
-
-									if ($method == 'post') {
-										$chOpts[CURLOPT_POST] = true;
-										$chOpts[CURLOPT_POSTFIELDS] = $data;
-										$chOpts[CURLOPT_URL] = $url;
-									}
-									else {
-										$url.='?'.is_array($data)?http_build_query($data):$data;
-										$chOpts[CURLOPT_URL] = $url;
-									}
-
-									curl_setopt_array($ch, $chOpts);
-									$response = curl_exec($ch);
-									curl_close($ch);
-									return $response;
-								}
-
-								if(isset($_POST['delete']))
-								{
-									$url = 'http://localhost:5000/postmethod';
-									http($url, json_encode(["type" => "delete", "id" => $_POST['delete']]), 'post');
-								} 
 
 								while($row = mysqli_fetch_array($result))
 								{
@@ -145,7 +109,7 @@
 														<a href="'.$row['url'].'"><t style="color:#ffffff;" class="text">'.$row['title'].'</t></a>
 													</div>													
 													<div class="col-lg-2 my-auto text-right">
-													<form action="index.php" id="delForm" method="post" role="form">
+													<form action="deleted.php" id="delForm" method="post" role="form">
 														<t style="color:#ffffff;" class="text">'.$dur.'</t>
 														    <button type="submit" id="delete" name="delete" value='.$vcount.' class="fas fa-times-circle white-text ml-5" style="border:0px transparent; background-color: transparent;" ></button>
 													</form>
